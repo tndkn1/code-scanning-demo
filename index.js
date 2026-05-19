ff1ebcb260b3b5a16a4643b839bdbeb1194117b6
tndk <tndkn1v@gmail.com>
2026-05-20 06:26:55 +0900

    Add Vercel deployment support

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
-const site = await Bun.file("./index.html").text();
+const site = fs.readFileSync(path.join(__dirname, "index.html"), "utf8");
 
-app.get("/", async (req, res) => {
-  let greet = site.replace("%%_USER_NAME%%", req.query.name);
+app.get("/", (req, res) => {
+  const greet = site.replace("%%_USER_NAME%%", req.query.name ?? "");
   res.send(greet);
 });
 
-app.listen(8080, () => {
-  console.log("The webpage is live on http://localhost:8080 :)");
-});
+if (require.main === module) {
+  const port = process.env.PORT || 8080;
+  app.listen(port, () => {
+    console.log(`The webpage is live on http://localhost:${port} :)`);
+  });
+}
+
+module.exports = app;
